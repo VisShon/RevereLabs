596133e188bbec18f56af54c0bde7be424ac718a
VisShon <vishnu20414@iiitd.ac.in>
2022-08-12 18:55:30 +0530

    link carousel done

--- components/LinkCarousel.jsx
+++ components/LinkCarousel.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Image from 'next/Image'
 
 function LinkCarousel() {
+
+
+  const linkData = [{title:'linkedIn',logo:'/logos/linkedIn.png'},
+  {title:'mail',logo:'/logos/mail.png'},
+  {title:'twitter',logo:'/logos/twitter.png'},
+  {title:'discord',logo:'/logos/discord.png'},
+  {title:'linktree',logo:'/logos/linktree.png'},]
+
+
+  const[newLink, setNewLink] = useState("")
+  const[userLinks,setUserLinks] = useState([])
+  const[addLink,setAddLink] = useState(false)
+
+  let data=[]
+
+  const onclickHandler = (title,logo,link) =>{
+    const newData = {title:title,logo:logo,link:link};
+    data.push(newData)
+    setUserLinks(data);
+    console.log(userLinks)
+    setAddLink(preVal=>!preVal);
+  }
+
   return (
-    <div>LinkCarousel</div>
+    <>
+      {userLinks.map((item,index)=>(<a key={index} href={item.link} className="w-[12rem] flex justify-between h-[2.5rem] z-[10] p-1 border-2 rounded-md cursor-pointer drop-shadow-[5px_5px_0px_rgba(0,0,0,1)]   bg-[#ffffff]">
+            <h2 className="text-textSecondary font-[600]">{item.title}</h2>
+            <Image src={item.logo} height={25} width={25} />
+      </a>))}
+
+      {
+        addLink?(<div className="relative w-[12rem] h-[2.5rem] z-[10] mt-5">
+                    <input value={newLink} onChange={(e)=>setNewLink(e.target.value)} placeholder="Search" type="search" className="p-1 relative z-[5] border-2 rounded-md cursor-text drop-shadow-[5px_5px_0px_rgba(0,0,0,1)]   bg-[#ffffff]"/>
+                    <div className="absolute pt-10 top-1 z-[0] w-[12rem] min-h-[5rem] flex items-center justify-around border-2 rounded-b-md font-mada bg-accent">
+                      {console.log(newLink)}
+                      {linkData.map((item, i)=>(
+                        <a onClick={()=>onclickHandler(item.title,item.logo,newLink)} key={i} className="drop-shadow-[2px_2px_0px_rgba(0,0,0,1)] active:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] cursor-pointer" >
+                          <Image src={item.logo} height={20} width={25} />
+                        </a>
+                      ))}
+                    </div>
+                </div>):
+
+
+                (<div onClick={()=>setAddLink(preVal=>!preVal)} className="w-[12rem] flex justify-center mt-5">
+                    <Image src={'/plus.png'} width={20} height={20}/>
+                </div>)
+      }
+    </>
   )
 }
 
--- pages/componentTester.jsx
+++ pages/componentTester.jsx
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
+import LinkCarousel from '../components/LinkCarousel'
 
 function componentTester() {
 
-    const[searchParam,setSearchParam] = useState("")
-
     return (
     <>
+      <LinkCarousel/>
     </>
   )
 }
