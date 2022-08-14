06e61de2eb9d048b979e2f1c81be6f16c80354d6
VisShon <vishnu20414@iiitd.ac.in>
2022-08-15 02:15:21 +0530

    updates

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react'
 import Progress from '../components/Progress'
-import LoginCard from '../components/LogInCard'
 import style from '../styles/LogInCard.module.scss'
 function Login() {
   const [stepsDone,setStepsDone] = useState(1);
@@ -10,7 +9,6 @@ function Login() {
       <div className="mb-10 w-[45rem]">
         <Progress steps={3} stepsDone={stepsDone}/>
       </div>
-      <LoginCard setStepsDone={setStepsDone} stepsDone={stepsDone}/>
     </div>
   )
 }
