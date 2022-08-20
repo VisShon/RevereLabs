bda118ef2d6983067399e97b7515ef4a2c6b76c2
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-20 22:12:40 +0530

    minor

---
+++ pages/api/gig-application/fetch.js
@@ -0,0 +1,20 @@
+import {fetchGigApplications, } from "../../../../functions/gig.js";
+
+export default async function handler(req, res) {
+    const query_params = {};
+    for (const key in req.query) {
+        query_params[key] = JSON.parse(req.query[key]);
+    }
+    const results = await fetchGigApplications(query_params);
+
+    res.status(200).json(results.map(gigApplication => {
+        return {
+            gigId: gigApplication.get("gigId"),
+            contractId: gigApplication.get("contractId"),
+            applicantId: gigApplication.get("applicantId"),
+            status: gigApplication.get("status"),
+            formalJson: gigApplication.get("formalJson"),
+        };
+    }
+    ));
+}
