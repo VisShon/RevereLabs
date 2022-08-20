79be08a67f28ad7684b9f985f8c34e0526ee3e26
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-20 11:51:16 +0530

    minor

--- pages/login.jsx
+++ pages/login.jsx
@@ -17,19 +17,23 @@ function Login() {
                     stepsDone={stepsDone}/>
                 <div className="relative z-10 mt-10 flex flex-col justify-center items-center w-[45rem] h-[30rem] bg-[#9DCEFB]  border-2 rounded-[5px] drop-shadow-[10px_10px_0px_rgba(0,0,0,1)]">
                     {stepsDone===1&&<>
-                        {login.Web3Providers.map((item, i)=>(<LoginButton APIlink={'#'}
-                            data={item}
-                            setStepsDone={setStepsDone}
-                            stepsDone={stepsDone}
-                            key={i}/>)) }
+                        {login.Web3Providers.map((item, i)=>(
+                            <LoginButton APIlink={'#'}
+                                data={item}
+                                setStepsDone={setStepsDone}
+                                stepsDone={stepsDone}
+                                key={i}/>
+                        )) }
                         <h2 className="font-mada font-[500] w-[60%] mt-10">We need your  Wallets for setting up the escrow services only when you sign an agreement.</h2>
                     </>}
                     {stepsDone===2&&<>
-                        {login.FaangProviders.map((item, i)=>(<LoginButton APIlink={'#'}
-                            data={item}
-                            setStepsDone={setStepsDone}
-                            stepsDone={stepsDone}
-                            key={i}/>)) }
+                        {login.FaangProviders.map((item, i)=>(
+                            <LoginButton APIlink={'#'}
+                                data={item}
+                                setStepsDone={setStepsDone}
+                                stepsDone={stepsDone}
+                                key={i}/>
+                        )) }
                         <h2 className="font-mada font-[500] w-[60%] mt-10">We need your google credentials to connect you to freelancers and identify you as a client.</h2>
                     </>}
                 </div>
