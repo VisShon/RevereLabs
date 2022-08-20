10083eea16fcf29178d9261f481ce5268cb37344
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-20 18:30:31 +0530

    gigid

--- pages/api/gig/create.js
+++ pages/api/gig/create.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
         title, description, bounty, time, issuedBy, category
     } = req.body;
     const result = await createGig(title, description, bounty, time, issuedBy, category);
+    console.log(result);
 
     res.status(201).json({id: result.id});
 }
