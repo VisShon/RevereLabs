a0134b90827dcd01facb436d867dda31d6b5a294
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-20 19:14:34 +0530

    new

--- pages/api/gig/get.js
+++ pages/api/gig/get.js
@@ -3,7 +3,7 @@ import {getGig, } from "../../../../functions/gig.js";
 export default async function handler(req, res) {
 
     let gigId = req.query.gigId;
-    const result = await getGig(gigId);
+    const result = (await getGig(gigId))[0];
     console.log(result);
     const title = result.get("title");
     const description = result.get("description");
