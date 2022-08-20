1ba640f20010a004376b1a58f0fd5be31b8ab820
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-08-20 17:24:21 +0530

    .

---
+++ pages/api/gig/create.js
@@ -0,0 +1,30 @@
+import {createOrUpdateUser, } from "../../../../functions/utils.js";
+
+export default async function handler(req, res) {
+
+    let {
+        email, name, skills, links, associations, additional, address, addType
+    } = req.body;
+    const [user, created] = await createOrUpdateUser(email, name, skills, links, associations, additional, address, addType);
+    if (created) {
+        // TODO: naman
+        // Send welcome email
+    }
+    else {
+        // TODO: naman
+        // Send prexisting user email
+    }
+
+    name = user.get("name");
+    email = user.get("email");
+    skills = user.get("skills");
+    links = user.get("links");
+    associations = user.get("associations");
+    address = user.get("address");
+    addType = user.get("addType");
+
+    console.log(user, user.get());
+
+
+    res.status(200).json({ name, skills, links, associations, email, created, address, addType });
+}
